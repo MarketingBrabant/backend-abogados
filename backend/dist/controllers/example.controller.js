ac9d07ba10b334b6bfaefd571b0ea55199c8e547
MarketingBrabant <marketing@lemornebrabant.com>
2025-06-11 11:37:51 +0200

    Corrige tipos globales de Express y error de crmToken

--- backend/dist/controllers/example.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getExample = void 0;
-const getExample = (_req, res) => {
-    res.json({ message: 'Ruta /example funcionando correctamente' });
-};
-exports.getExample = getExample;
